1e0914a2992e02b73db64697b2eaa07e2eee77c0
Kat <105393029+surfergrl@users.noreply.github.com>
2025-01-09 13:38:30 +0000

    Adding favicons to images folder and template

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-install os 
-from flask import Flask
+import os 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/templates/index.html")
+    return render_template("/index.html")
 
 
 @app.route("/")
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=true
+        debug=True
     )
