f0a43f330c91dbeccdb38aedb5c7ba47096ed079
Kat <105393029+surfergrl@users.noreply.github.com>
2025-01-14 20:21:50 +0000

    Routing not working

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("/static/node_modules/startbootstrap-small-business/dist/index.html")
+    return render_template("/templates/index.html")
 
 
 @app.route("/")
