d9e21ac9ba77dfc7506945bd700ec8f03eef8735
Kat <105393029+surfergrl@users.noreply.github.com>
2025-01-16 12:37:53 +0000

    Add login page and map route

--- run.py
+++ run.py
@@ -33,6 +33,9 @@ def contact():
 def services():
     return render_template("/services.html")
 
+@app.route("/login")
+def login():
+    return render_template("/login.html")
 
 if __name__ == "__main__":
     app.run(
